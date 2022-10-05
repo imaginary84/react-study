740355a11b3ea1530b2ccb02ff587b1e284b06aa
imaginary84 <imaginary84@gmail.com>
2022-10-06 00:10:45 +0900

    Reducer 사용하기

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import './index.css';
 // import App3 from './App3';
 import App4 from 'App4';
 import App5 from 'App5';
+import App6 from 'App6';
 import EpisodeList from './EpisodeList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App5 />);
+root.render(<App6 />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
